37778c19c92098f8e9a2785eb3ff2c1123583a73
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2026-05-13 08:22:18 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import { router } from "@/app/app-router"
-import { RouterProvider } from "react-router-dom"
-import { Toaster } from "react-hot-toast"
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { router } from "@/app/app-router";
+import { RouterProvider } from "react-router-dom";
+
+import { Toaster } from "react-hot-toast";
+
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
-  <>
+  <HelmetProvider>
     <RouterProvider router={router} />
     <Toaster position="top-right" reverseOrder={false} />
-  </>
-)
+  </HelmetProvider>
+);
